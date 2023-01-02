14ba1e3c30edecff39861e475e48cde8bf2709ec
Connor White <connor.bcw@gmail.com>
2023-01-02 09:13:31 -0600

    gitignore fix

--- .auto/build/bin/index.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-import { autobin } from "../index.js";
-autobin.parse();
--- .auto/build/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const autobin: import("clee").Command<"autobin", [{
-    hashbang: `#!${string}` | undefined;
-}], Promise<void>, undefined>;
